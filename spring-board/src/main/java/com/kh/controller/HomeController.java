ef6524b518f90b7e62b9a3b0d138bffdf46a5a62
Choi-Seungin <chltmddls1@gmail.com>
2025-01-06 17:28:54 +0900

    20250106

---
+++ spring-board/src/main/java/com/kh/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.kh.controller;
+
+import java.security.Provider.Service;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.dto.BoardDTO;
+import com.kh.service.BoardService;
+import com.kh.vo.PaggingVO;
+
+@Controller
+public class HomeController {
+	private BoardService service;
+
+	public HomeController(BoardService service) {
+		this.service = service;
+	}
+
+	 @GetMapping("/")
+	  public ModelAndView index(@RequestParam(defaultValue = "1") int pageNo,
+	    @RequestParam(defaultValue = "30") int pageContentEa, ModelAndView view) {
+	      //전체 게시글 개수 조회
+	      int count = service.selectBoardTotalCount();
+	      //페이지 번호를 보내서 해당 페이지 게시글 목록만 조회
+	      List<BoardDTO> list = service.getBoardList(pageNo, pageContentEa);
+	      //PaggingVO 페이징 정보 생성
+	      PaggingVO pagging = new PaggingVO(count, pageNo, pageContentEa);
+	      //데이터 추가
+	      view.addObject("list", list);
+	      view.addObject("pagging", pagging);
+	      //이동할 페이지 설정
+	      view.setViewName("index");
+
+	    return view;
+	  }
+
+}
