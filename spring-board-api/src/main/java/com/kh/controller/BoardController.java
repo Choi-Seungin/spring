c8aaf46ae0c6f65a207bb7b844aa7dcb93b4eadf
Choi-Seungin <chltmddls1@gmail.com>
2025-01-17 17:51:54 +0900

    20250117

--- spring-board-api/src/main/java/com/kh/controller/BoardController.java
+++ spring-board-api/src/main/java/com/kh/controller/BoardController.java
@@ -1,14 +1,20 @@
 package com.kh.controller;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +23,12 @@ import org.springframework.web.servlet.ModelAndView;
 import com.kh.dto.BoardCommentDTO;
 import com.kh.dto.BoardDTO;
 import com.kh.dto.BoardFileDTO;
+import com.kh.dto.BoardMemberDTO;
 import com.kh.service.BoardService;
 import com.kh.token.JwtTokenProvider;
 import com.kh.vo.PaggingVO;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
@@ -70,5 +78,175 @@ public class BoardController {
 	public List<BoardCommentDTO> getMethodName(@PathVariable int bno, @RequestParam int start) {
 		List<BoardCommentDTO> commentList = boardService.getCommentList(bno, start);
 		return commentList;
+	}
+
+	@GetMapping("/board/like/{bno}")
+	public Map<String, Object> boardLike(@PathVariable int bno, @RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token == null) {
+			map.put("code", 2);
+			map.put("msg", "로그인 하셔야 이용하실수 있습니다.");
+		} else {
+			String id = tokenProvider.getUserIDFromToken(token);
+			try {
+				boardService.insertBoardLike(bno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 게시글에 좋아요 하셨습니다.");
+			} catch (Exception e) {
+				boardService.deleteBoardLike(bno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 게시글에 좋아요를 취소 하셨습니다.");
+			}
+			map.put("count", boardService.getBoardLike(bno));
+		}
+		return map;
+	}
+
+	@GetMapping("/board/hate/{bno}")
+	public Map<String, Object> boardHate(@PathVariable int bno, @RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token == null) {
+			map.put("code", 2);
+			map.put("msg", "로그인 하셔야 이용하실수 있습니다.");
+		} else {
+			String id = tokenProvider.getUserIDFromToken(token);
+			try {
+				boardService.insertBoardHate(bno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 게시글에 싫어요 하셨습니다.");
+			} catch (Exception e) {
+				boardService.deleteBoardHate(bno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 게시글에 싫어요를 취소 하셨습니다.");
+			}
+			map.put("count", boardService.getBoardHate(bno));
+		}
+		return map;
+	}
+
+	@GetMapping("/board/comment/like/{cno}")
+	public Map<String, Object> boardCommentLike(@PathVariable int cno, @RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token == null) {
+			map.put("code", 2);
+			map.put("msg", "로그인 하셔야 이용하실수 있습니다.");
+		} else {
+			String id = tokenProvider.getUserIDFromToken(token);
+			try {
+				boardService.insertBoardCommentLike(cno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 댓글에 좋아요 하셨습니다.");
+			} catch (Exception e) {
+				boardService.deleteBoardCommentLike(cno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 댓글에 좋아요를 취소 하셨습니다.");
+			}
+			map.put("count", boardService.selectCommentLikeCount(cno));
+		}
+		return map;
+	}
+
+	@GetMapping("/board/comment/hate/{cno}")
+	public Map<String, Object> boardCommentHate(@PathVariable int cno, @RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token == null) {
+			map.put("code", 2);
+			map.put("msg", "로그인 하셔야 이용하실수 있습니다.");
+		} else {
+			String id = tokenProvider.getUserIDFromToken(token);
+			try {
+				boardService.insertBoardCommentHate(cno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 댓글에 싫어요 하셨습니다.");
+			} catch (Exception e) {
+				boardService.deleteBoardCommentHate(cno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 댓글에 싫어요를 취소 하셨습니다.");
+			}
+			map.put("count", boardService.selectCommentHateCount(cno));
+		}
+		return map;
+	}
+
+	@ResponseBody
+	@PostMapping("/board/comment")
+	public Map<String, Object> boardCommentWrite(@RequestBody Map<String, Object> map,
+			@RequestHeader("Authorization") String token) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		// 로그인하지 않았을때
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token == null) {
+			result.put("code", 2);
+			result.put("msg", "로그인 하셔야 이용하실 수 있습니다.");
+		} else {
+			String id = tokenProvider.getUserIDFromToken(token);
+			BoardCommentDTO comment = new BoardCommentDTO(Integer.parseInt(map.get("bno").toString()), id,
+					map.get("content").toString());
+			boardService.insertBoardComment(comment);
+			result.put("code", 1);
+			result.put("msg", "댓글이 작성되었습니다.");
+			result.put("commentList", boardService.getCommentList(comment.getBno(), 1));
+		}
+		return result;
+	}
+
+	@DeleteMapping("/board/{bno}")
+	public Map<String, Object> boardDelete(@PathVariable int bno, @RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token != null && tokenProvider.getUserIDFromToken(token).equals(boardService.selectBoard(bno).getId())) {
+			List<BoardFileDTO> fileList = boardService.getBoardFileList(bno);
+			fileList.forEach(file -> {
+				File f = new File(file.getFpath());
+				f.delete();// 물리적으로 삭제
+			});
+			boardService.deleteBoard(bno);
+			map.put("code", 1);
+			map.put("msg", "해당 게시글 삭제를 완료하였습니다.");
+		} else {
+			map.put("code", 2);
+			map.put("msg", "게시글 삭제를 실패하였습니다.");
+		}
+		return map;
+	}
+	
+	@PutMapping("/board/comment")
+	public Map<String, Object> boardCommentUpdate(@RequestBody Map<String, String> body,
+			@RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		BoardCommentDTO comment = boardService.selectComment(Integer.parseInt(body.get("cno")));
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token != null && tokenProvider.getUserIDFromToken(token).equals(comment.getId())) {
+			comment.setContent(body.get("content"));
+			boardService.updateBoardComment(comment);
+			map.put("code", 1);
+			map.put("msg", "해당 댓글 수정 완료");
+			map.put("commentList", boardService.getCommentList(comment.getBno(), 1));
+		} else {
+			map.put("code", 2);
+			map.put("msg", "해당 댓글 작성자만 수정이 가능합니다.");
+		}
+		return map;
+	}
+	
+	@DeleteMapping("/board/comment/{cno}")
+	public Map<String, Object> commentDelete(@PathVariable int cno, @RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		BoardCommentDTO comment = boardService.selectComment(cno);
+		if (token != null && tokenProvider.getUserIDFromToken(token).equals(comment.getId())) {
+			boardService.deleteBoardComment(cno);
+			map.put("code", 1);
+			map.put("msg", "해당 댓글 삭제를 완료하였습니다.");
+			map.put("commentList", boardService.getCommentList(comment.getBno(), 1));
+		} else {
+			map.put("code", 2);
+			map.put("msg", "댓글 삭제를 실패하였습니다.");
+		}
+		return map;
 	}
 }
